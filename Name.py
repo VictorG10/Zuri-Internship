344fd96a3950b0abe295d274f4d840cce668b760
VictorG10 <victorgbadebo10@gmail.com>
2021-08-20 08:28:44 +0100

    first commit

---
+++ Name.py
@@ -0,0 +1,6 @@
+def personal_details():
+    name, age = "Gbadebo Victor", 24
+    address = "Lagos, Nigeria"
+    print("Name: {}\nAge: {}\nAddress: {}".format(name, age, address))
+
+personal_details()
